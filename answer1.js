4bbf30a598b465e75794a1eb465f9c98f9ebf315
haa-meem <haa-meem@users.noreply.github.com>
2026-09-02 00:37:10 +0600

    question and answer indivisual files are deleted and merged into answer.js

--- answer1.js
+++
@@ -1,8 +0,0 @@
-function describeValue(value) {
-    if (value === "" || value === 0 || value === null || value === undefined) {
-        return `${typeof (value)} | falsy`;
-    }
-    else {
-        return `${typeof (value)} | truthy`;
-    }
-}
